99e6bb46aa872fdb46d1540f5cc2777ab8abb420
Chandan Gupta <34454145+chandan78@users.noreply.github.com>
2018-10-31 15:42:13 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SignUp</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/login.css" type="text/css"></link>
+		<script src="js/login.js"></script>
+</head>
+	<body>	
+				<div class="simple-form">
+					<form class="registration" method="post" name="regno" >
+						<h2 id="h21">Sign-In</h2>
+						<input type="text" id="username" class="button" placeholder="Enter Your UserName" required><br><br>         
+						<input type="password" id="password" class="button" placeholder="Enter Your Password" required><br><br>
+						<input type="button" value="Login" id="butt" onclick="validate()"/>             
+					</form>
+				</div>
+	</body>
+</html>
